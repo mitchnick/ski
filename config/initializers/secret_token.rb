79fb18841b25cabc921042f1ceec10436a82d7e9
mitchnick <mitchnick@gmail.com>
2013-01-29 10:26:55 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wheretoski::Application.config.secret_token = '5a546e2a4c1993b139d34de3d485a5f9bedc6aa52ee2a62d720b1fbb0c79d86dc8fda1486f8e3952abae05432c463bf5b3de856ded0eb539783cf7b9841962a5'
