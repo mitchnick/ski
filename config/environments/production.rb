2ad37d888ab651275f4c69e6c3f59c76bf1e3d91
mitchnick <mitchnick@gmail.com>
2013-04-29 15:17:23 -0500

    add application variables upon load

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ Wheretoski::Application.configure do
 
   config.action_mailer.smtp_settings = {
     address: "smtpout.secureserver.net",
-    port: 465,
+    port: 80,
     domain: "wheretoski.co",
     authentication: "plain",
     enable_starttls_auto: true,
