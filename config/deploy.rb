ba4745443fec1e367df64099123e47a49c5e50e8
mitchnick <mitchnick@gmail.com>
2013-04-30 14:19:19 -0500

    update so cap deploy:setup doesn't fail

--- config/deploy.rb
+++ config/deploy.rb
@@ -38,6 +38,12 @@ namespace :deploy do
     sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
     sudo "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_#{application}"
     run "mkdir -p #{shared_path}/config"
+
+    # custom my mitch
+    run "mkdir -p #{release_path}/config"
+    run ">#{release_path}/config/application.yml"
+    run "ln -s #{shared_path}/config/application.yml #{release_path}/config/application.yml"
+
     put File.read("config/database.yml"), "#{shared_path}/config/database.yml"
     puts "Now edit the config files in #{shared_path}."
   end
@@ -47,12 +53,6 @@ namespace :deploy do
     run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
   end
   after "deploy:finalize_update", "deploy:symlink_config"
-
-  task :setup do 
-    run "mkdir -p #{release_path}/config"
-    run ">#{release_path}/config/application.yml"
-    run "ln -s #{shared_path}/config/application.yml #{release_path}/config/application.yml"
-  end
 
   before 'deploy:assets:precompile' do
     run "ln -s #{shared_path}/config/application.yml #{release_path}/config/application.yml"
