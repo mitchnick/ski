54a7e319f6e6d4c1730e8f7619fa7a19656d1f89
mitchnick <mitchnick@gmail.com>
2013-05-08 17:16:50 -0500

    google analytics update

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,19 @@
 module ApplicationHelper
 
 # Helps set the page title across the application 
+	def google_analytics(id)
+    content_tag :script, :type => 'text/javascript' do
+      "var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', '#{id}']);
+      _gaq.push(['_trackPageview']);
+      (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+      })();".html_safe
+    end if id && Rails.env == 'production'
+  end
+
 	def full_title(page_title)
 		base_title = "Where to Ski"
 		if page_title.empty?
